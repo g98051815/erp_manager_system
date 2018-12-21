5239252db93726891d553e22a8db0fa56d1d8d10
mahuiqiang <1393286490@qq.com>
2018-12-21 18:56:20 +0800

    创建api项目

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	_ "erp_manager_system/routers"
+
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
